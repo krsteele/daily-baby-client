7f9cd2bcab4715ed2c49c4701e81cdc148690ab2
Kristen Steele <ksteelephoto@gmail.com>
2021-03-19 12:06:24 -0500

    imports and renders DailyBaby componnet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
+import { DailyBaby } from "./components/DailyBaby"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <DailyBaby />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
