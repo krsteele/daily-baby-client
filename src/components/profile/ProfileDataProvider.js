90bf5c8330bb120d2986a5f4ff57c8c3c3eeea04
Kristen Steele <ksteelephoto@gmail.com>
2021-03-19 15:26:54 -0500

    adds file structure for profile component

---
+++ src/components/profile/ProfileDataProvider.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react"
+
+export const ProfileContext = React.createContext()
+
+export const ProfileDataProvider = (props) => {
+    const [profile, setProfile] = useState([])
+
+    
+}
