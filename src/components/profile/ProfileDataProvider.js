810a2d8ef13ab1b311bd7360af082df98667ce53
Kristen Steele <ksteelephoto@gmail.com>
2021-03-19 15:52:45 -0500

    adds getProfile and updateProfile to ProfileDataProvider

--- src/components/profile/ProfileDataProvider.js
+++ src/components/profile/ProfileDataProvider.js
@@ -5,5 +5,36 @@ export const ProfileContext = React.createContext()
 export const ProfileDataProvider = (props) => {
     const [profile, setProfile] = useState([])
 
-    
+    const getProfile = () => {
+        return fetch("http://localhost:8000/profile", {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("db_token")}`,
+            },
+        })
+            .then(res => res.json())
+            .then(setProfile)
+    }
+
+    const updateProfile = (profile) => {
+        return fetch(`http://localhost:8000/users/${profile.user.id}`, {
+            method: "PUT",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("db_token")}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(profile),
+            }).then(getProfile)
+        }
+
+        return (
+            <ProfileContext.Provider
+                value={{
+                    getProfile,
+                    Profile,
+                    updateProfile
+                }}
+                >
+                {props.children}
+                </ProfileContext.Provider>
+            )
 }
