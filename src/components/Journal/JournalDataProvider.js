3b8d37e03086e73072a0f91be1620a9b5002f63d
Kristen Steele <ksteelephoto@gmail.com>
2021-03-20 14:44:26 -0500

    adds JournalDataProvider with getEntries by babyId

---
+++ src/components/Journal/JournalDataProvider.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react"
+
+export const JournalContext = React.createContext()
+
+export const JournalDataProvider = (props) => {
+    const [entries, setEntries] = useState([])
+
+    const getJournal = (id) => {
+        return fetch(`http://localhost:8000/entries?babyId=${id}`, {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("db_token")}`,
+            },
+        })
+            .then(res => res.json())
+            .then(setEntries)
+    }
+
+        return (
+            <JournalContext.Provider
+                value={{
+                    getJournal,
+                    entries
+                }}
+                >
+                {props.children}
+                </JournalContext.Provider>
+            )
+}
