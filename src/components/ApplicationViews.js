a092e7e413f55e2df2efc69bd0536541c0e0586c
Kristen Steele <ksteelephoto@gmail.com>
2021-03-21 11:18:26 -0500

    imports JournalForm

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,6 +5,7 @@ import { ProfileDetail } from "./profile/ProfileDetail"
 import { BabyDataProvider } from "./baby/BabyDataProvider"
 import { JournalDataProvider } from "./Journal/JournalDataProvider"
 import { JournalList } from "./Journal/JournalList"
+import { JournalForm } from "./Journal/JournalForm"
 
 
 export const ApplicationViews = () => {
