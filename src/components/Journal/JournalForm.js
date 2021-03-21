6b6a20fe1c59fbb85809a9f2675abacfe41048e7
Kristen Steele <ksteelephoto@gmail.com>
2021-03-21 13:24:33 -0500

    dropdown works

--- src/components/Journal/JournalForm.js
+++ src/components/Journal/JournalForm.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react"
 
 import { JournalContext } from "./JournalDataProvider"
+import { BabyContext } from "../baby/BabyDataProvider"
 
 // React-Hook-Form
 import { useForm } from "react-hook-form"
@@ -12,22 +13,45 @@ import Container from "react-bootstrap/Container"
 
 export const JournalForm = (props) => {
     const { getEntry, addJournalEntry, updateJournalEntry } = useContext(JournalContext)
+    const { getBabies, babies } = useContext(BabyContext)
+    
     const [entry, setEntry] = useState({photo: {}, user_baby: {baby: {}, user: {user: {}}}})
+
 
     //  Grab needed functions from React-Form-Hook
     const { register, handleSubmit, errors, formState } = useForm()
 
     // Get data needed to render dropdown
-    // - need children by user
-
+    useEffect(() => {
+        getBabies()
+    }, [])
     // Check for edit mode
     // If edit mode, get and set entry to be updated
 
     // Build function to create or update entry
+    const entryAddOrUpdate = (data) => {
+        console.log("Someone clicked my button!", data)
+    }
 
+    console.log(babies)
     return (
         <Container>
-            <div>Hey, I'm the journal entry form!</div>
+            <h2>New Journal Entry</h2>
+            <Form onSubmit={handleSubmit(entryAddOrUpdate)}>
+                <Form.Group controlId="form__baby">
+                    <Form.Label>Child</Form.Label>
+                    <Form.Control ref={register({valueAsNumber: true})} name="babyId" as="select">
+                    <option value="null">Who is your entry about?</option>
+                    {
+                        babies.map(baby => (
+                            <option key={baby.baby.id} value={baby.baby.id}>{baby.baby.first_name} {baby.baby.middle_name} {baby.baby.last_name}</option>
+                        ))
+                    }
+                    </Form.Control>
+                </Form.Group>
+                <Button className="btn" variant="primary" type="submit" disabled={formState.isSubmitting}>Submit</Button>
+            </Form>
+
         </Container>
     )
 
