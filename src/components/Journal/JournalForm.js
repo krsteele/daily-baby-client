883a6c83eabbe65cbecb6be0c20883d0b70f7cf2
Kristen Steele <ksteelephoto@gmail.com>
2021-03-21 22:53:39 -0500

    implementing update

--- src/components/Journal/JournalForm.js
+++ src/components/Journal/JournalForm.js
@@ -10,7 +10,6 @@ import { useForm } from "react-hook-form"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import Container from "react-bootstrap/Container"
-import { render } from "@testing-library/react"
 
 export const JournalForm = (props) => {
     const { getEntry, addJournalEntry, updateJournalEntry } = useContext(JournalContext)
@@ -18,6 +17,7 @@ export const JournalForm = (props) => {
     
     const [entry, setEntry] = useState({photo: {}, user_baby: {baby: {}, user: {user: {}}}})
     const [image, setImage] = useState("")
+    const [editModeImage, setEditModeImage] = useState("")
     const [loading, setLoading] = useState(false)
 
 
@@ -30,10 +30,42 @@ export const JournalForm = (props) => {
     }, [])
     // Check for edit mode
     // If edit mode, get and set entry to be updated
+    const editMode = props.match.params.hasOwnProperty("entryId")
+    const entryId = props.match.params.entryId
 
-    // Build function to create or update entry
+    const getEntryInEditMode = () => {
+        if (editMode) {
+            getEntry(entryId).then(setEntry);
+        }
+    };
+
+    useEffect(() => {
+        getEntryInEditMode();
+    }, [babies]);
+
+
+    // Called on form submit to create or edit the entry
     const entryAddOrUpdate = (data) => {
         console.log("Someone clicked my button!", data)
+        if (editMode) {
+            data.image = image
+            data.id = entryId
+            data.created_on = entry.created_on
+            data.is_private = entry.is_private
+            data.prompt = entry.prompt
+            data.userBaby = entry.user_baby.id
+            updateJournalEntry(data)
+                .then(() => props.history.push(`/journal/${data.babyId}`))
+        } else {
+            addJournalEntry({
+                text: data.entryText,
+                babyId: data.babyId,
+                image: image,
+                is_private: false,
+                prompt: 1
+            })
+            .then(() => props.history.push(`/journal/${data.babyId}`))
+        }
     }
 
     const uploadImage = async (e) => {
@@ -65,8 +97,8 @@ export const JournalForm = (props) => {
             <Form onSubmit={handleSubmit(entryAddOrUpdate)}>
                 <Form.Group controlId="form__baby">
                     <Form.Label>Child</Form.Label>
-                    <Form.Control ref={register({valueAsNumber: true})} name="babyId" as="select">
-                    <option value="null">Who is your entry about?</option>
+                    <Form.Control ref={register({valueAsNumber: true})} name="babyId" as="select" defaultValue={entry.user_baby.baby.nickname}>
+                    <option key="key" value="null">Who is your entry about?</option>
                     {
                         babies.map(baby => (
                             <option key={baby.baby.id} value={baby.baby.id}>{baby.baby.first_name} {baby.baby.middle_name} {baby.baby.last_name}</option>
@@ -75,7 +107,7 @@ export const JournalForm = (props) => {
                     </Form.Control>
                 </Form.Group>
                 <Form.Group>
-                    <Form.File ref={register} name="entryImage" id="entryImage" label="Upload an image" onChange={uploadImage} />
+                    <Form.File ref={register} name="entryImage" id="entryImage" label="Upload an image" onChange={uploadImage} defaultValue={entry.photo.image} />
                     {loading ? (
                         <h3>Loading...</h3>
                     ) : (
@@ -84,7 +116,7 @@ export const JournalForm = (props) => {
                 </Form.Group>
                 <Form.Group controlId="form__entry">
                     <Form.Label>What wonderful things are happening in your baby's life right now?</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control as="textarea" rows={3} name="entryText" ref={register} defaultValue={entry.text} />
                 </Form.Group>
                 <Button className="btn" variant="primary" type="submit" disabled={formState.isSubmitting}>Submit</Button>
             </Form>
