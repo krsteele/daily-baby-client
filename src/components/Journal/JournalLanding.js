b8e393639d99240e9ca843d2b8e5b5efbe42415a
Kristen Steele <ksteelephoto@gmail.com>
2021-04-07 14:21:10 -0500

    fixes spacing of journal entry form layout, adds flex to journal cards on landing page

--- src/components/Journal/JournalLanding.js
+++ src/components/Journal/JournalLanding.js
@@ -15,6 +15,8 @@ import Image from 'react-bootstrap/Image'
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
+import "./journal.css"
+
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
@@ -35,35 +37,36 @@ export const JournalLanding = (props) => {
     }, [])
     
     return(
-        <Container>
-            
+        <main>
+            <section className="journal">
+                
                 <h1>Journals</h1>
+                <Route render={props => <AddChildButton {...props} />} />
                 
                 <div className="journal__children">
-                <h3></h3>
-                <Route render={props => <AddChildButton {...props} />} />
-                {
-                    profile.userbabies.length > 0 
-                    ?
-                    profile.userbabies.map(baby => {
-                        return (
-                            <Card border="primary" className="text-center" style={{ width: '18rem' }} key={baby.baby.id} onClick={() => history.push(`/journal/${baby.baby.id}`)}>
-                                <Card.Header>View the journal for</Card.Header>
-                                <Card.Header>{baby.baby.first_name} {baby.baby.middle_name} {baby.baby.last_name}</Card.Header>
-                                <Card.Body>
-                                    <Image src={baby.baby.profile_image} roundedCircle />
-                                </Card.Body>
-                                <Card.Body>
-                                </Card.Body>
-                            </Card>
-                        )
-                    })
-                    :
-                    <Alert variant="secondary">Please 
-                            <Alert.Link href="/children/create">add a child</Alert.Link> to begin your journal.</Alert>
-                }
-            </div>
+                    {
+                        profile.userbabies.length > 0 
+                        ?
+                        profile.userbabies.map(baby => {
+                            return (
+                                <Card border="primary" className="text-center journal__child" style={{ width: '18rem' }} key={baby.baby.id} onClick={() => history.push(`/journal/${baby.baby.id}`)}>
+                                    <Card.Header>View the journal for</Card.Header>
+                                    <Card.Header>{baby.baby.first_name} {baby.baby.middle_name} {baby.baby.last_name}</Card.Header>
+                                    <Card.Body>
+                                        <Image src={baby.baby.profile_image} roundedCircle />
+                                    </Card.Body>
+                                    <Card.Body>
+                                    </Card.Body>
+                                </Card>
+                            )
+                        })
+                        :
+                        <Alert variant="secondary">Please 
+                                <Alert.Link href="/children/create">add a child</Alert.Link> to begin your journal.</Alert>
+                    }
+                </div>
                 
-        </Container>
+            </section>
+        </main>
     )
 }
