fa607927cf7d6a79f6d85c5151819f3a3668b6dd
Kristen Steele <ksteelephoto@gmail.com>
2021-04-07 08:42:08 -0500

    add logo image to navbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,19 +6,31 @@ import { AddEntryButton } from "../Journal/AddEntryButton"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 
+import Logo from '../../images/daily-baby-logo-trans.png'
+
 
 export const NavBar = (props) => {
     return (
         <>
             <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">
-                Daily Baby
+            <Navbar.Brand href="/" className="my-auto">
+                <img
+                    src={Logo}
+                    height="50"
+                    className="d-inline-block align-top"
+                    alt="Daily Baby logo"
+                />
+                <Navbar.Text>
+                    Daily Baby
+                </Navbar.Text>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                 <Nav className="mr-auto">
                     <Nav.Link href="/">Journals</Nav.Link>
                     <Nav.Link href="/profile">Profile</Nav.Link>
+                </Nav>
+                <Nav className="justify-content-end">
                     <Route render={props => <AddEntryButton {...props} />} />
                     <Route render={props => <Logout {...props} />} />
                 </Nav>
