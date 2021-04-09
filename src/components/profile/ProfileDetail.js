8f4cc5b72186065d1d43fc73ded04e79e5d37035
Kristen Steele <ksteelephoto@gmail.com>
2021-04-09 12:26:04 -0500

    styles up profile detail reaaaaal nice.

--- src/components/profile/ProfileDetail.js
+++ src/components/profile/ProfileDetail.js
@@ -69,71 +69,91 @@ export const ProfileDetail = (props) => {
     return(
         <main>
             <div className="profile">
-
-            <div className="profile__personal">
                 <h1>Profile</h1>
-                <Image src={profile.dailyuser.profile_image} roundedCircle />
                 <Button className="btn" variant="primary" type="button" onClick={() => history.push("/profile/edit")} >Edit Profile & Preferences</Button>
-                <h3>Personal Information</h3>
-                <p><b>Name:</b> {profile.dailyuser.user.first_name} {profile.dailyuser.user.last_name}</p>
-                <p><b>Username:</b> {profile.dailyuser.user.username}</p>
-                <p><b>Email:</b> {profile.dailyuser.user.email}</p>
-            </div>
-            <div className="profile__preferences">
-                <h3>Preferences</h3>
-                <p><b>You'll receive text message reminders</b></p>
-                <p><b>At this number:</b> {profile.dailyuser.phone_number}</p>
-                <p><b>At this time:</b> {profile.dailyuser.text_time}</p>
-                <p><b>On these days: </b></p>
-                    {
-                        selectedDays.map(day => {
-                            return <p>{day}</p>
-                        })
-                    }
-            </div>
-            <div>
-                <h3>Children</h3>
-                <Route render={props => <AddChildButton {...props} />} />
-                <div className="profile__children">
 
-                
-                {
-                    profile.userbabies.length === 0 
-                    ?
-                    <Alert variant="secondary">Please {" "}
-                            <Alert.Link href="/children/create">add a child</Alert.Link> to begin your journal.</Alert>
-                    :
-                    profile.userbabies.map(baby => {
-                        return (
-                            <Card border="primary" className="profile__child text-center" style={{ width: '18rem' }} key={baby.baby.id}>
-                                <Card.Header>{baby.baby.first_name} {baby.baby.middle_name} {baby.baby.last_name}</Card.Header>
-                                <Card.Body>
-                                    <Image src={baby.baby.profile_image} roundedCircle />
-                                </Card.Body>
-                                <Card.Body>
-                                <Card.Text>
-                                    Born {baby.baby.birth_date}
-                                </Card.Text>
-                                </Card.Body>
-                                <Card.Body> 
-                                    <Row>
-                                        <Col>
-                                            <FontAwesomeIcon icon={faBookOpen} onClick={() => props.history.push(`/journal/${baby.baby.id}`)} />
-                                        </Col>
-                                        <Col>
-                                            <FontAwesomeIcon icon={faEdit} onClick={() => props.history.push(`/children/edit/${baby.baby.id}`)} />
-                                        </Col>
-                                        <Col>
-                                            <FontAwesomeIcon icon={faPlusCircle} onClick={() => props.history.push("/journal/create")} />
-                                        </Col>
-                                    </Row>
-                                </Card.Body>
-                            </Card>
-                        )
-                    })
-                }
-            </div>
-            </div>
+                <div className="profile__info profile__section">
+                    <div className="info--personal info--card card border-primary">
+                        <h3 className="card-header">Personal Information</h3>
+                        <div className="card-body">
+                            <div className="personal--content">
+                                <Image className="profile--image" src={profile.dailyuser.profile_image} roundedCircle />
+                                <div className="content--text">
+                                    <p><b>Name:</b> {profile.dailyuser.user.first_name} {profile.dailyuser.user.last_name}</p>
+                                    <p><b>Username:</b> {profile.dailyuser.user.username}</p>
+                                    <p><b>Email:</b> {profile.dailyuser.user.email}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="info--preferences info--card card border-primary">
+                        <h3 className="card-header">Preferences</h3>
+                        <div className="card-body">
+                            <p><b>You'll receive text message reminders</b></p>
+                            <p><b>At this number:</b> {profile.dailyuser.phone_number}</p>
+                            <p><b>At this time:</b> {profile.dailyuser.text_time}</p>
+                            <p><b>On these days: </b></p>
+                            <div className="checkbox__container">
+
+                            {
+                                selectedDays.map(day => {
+                                    return <p>{day}</p>
+                                })
+                            }
+                            </div>
+                        </div>    
+                    </div>
+
+                </div>
+
+                    <div className="info--card card border-primary">
+                        <h3 className="card-header">Children</h3>
+                        <div className="card-body">
+
+                        <Route render={props => <AddChildButton {...props} />} />
+                        <div className="profile__children profile__section">
+
+                        
+                        
+                            {
+                                profile.userbabies.length === 0 
+                                ?
+                                <Alert variant="secondary">Please {" "}
+                                        <Alert.Link href="/children/create">add a child</Alert.Link> to begin your journal.</Alert>
+                                :
+                                profile.userbabies.map(baby => {
+                                    return (
+                                        <Card border="primary" className="profile__child text-center" style={{ width: '18rem' }} key={baby.baby.id}>
+                                            <Card.Header>{baby.baby.first_name} {baby.baby.middle_name} {baby.baby.last_name}</Card.Header>
+                                            <Card.Body>
+                                                <Image src={baby.baby.profile_image} roundedCircle />
+                                            </Card.Body>
+                                            <Card.Body>
+                                            <Card.Text>
+                                                Born {baby.baby.birth_date}
+                                            </Card.Text>
+                                            </Card.Body>
+                                            <Card.Body> 
+                                                <Row>
+                                                    <Col>
+                                                        <FontAwesomeIcon icon={faBookOpen} onClick={() => props.history.push(`/journal/${baby.baby.id}`)} />
+                                                    </Col>
+                                                    <Col>
+                                                        <FontAwesomeIcon icon={faEdit} onClick={() => props.history.push(`/children/edit/${baby.baby.id}`)} />
+                                                    </Col>
+                                                    <Col>
+                                                        <FontAwesomeIcon icon={faPlusCircle} onClick={() => props.history.push("/journal/create")} />
+                                                    </Col>
+                                                </Row>
+                                            </Card.Body>
+                                        </Card>
+                                    )
+                                })
+                            }
+                        </div>
+                        </div>
+                    </div>
             </div>
         </main>
     )
